0e51f72d3473467f72f6a76e2d35d9e210d5ca8d
manish-gokhare <59787265+manish-gokhare@users.noreply.github.com>
2025-07-20 12:14:50 +0200

    Update user_creation.sh

--- user_creation.sh
+++ user_creation.sh
@@ -44,3 +44,30 @@ sudo systemctl start jupyterhub
 sudo systemctl status jupyterhub
 
 
+#Test
+
+conda activate myenvs
+python -m ipykernel install --name "myenvs" --display-name "Python (myenvs)" --sys-prefix
+
+Above command install It installs the kernel in and JupyterHub automatically scans Conda envs with --sys-prefix for shared kernels in
+~/miniforge/envs/myenvs/share/jupyter/kernels/myenvs/
+
+chmod -R o+rx ~/miniforge
+
+# JupyterHub automatically detects system-wide kernels, including:
+
+# 1) Kernels in /usr/local/share/jupyter/kernels
+
+# 2) Kernels in Conda envs registered via --sys-prefix
+
+# So if you're using the correct --sys-prefix option, you do not need to do anything else.
+
+To confirm it’s detected:
+jupyter kernelspec list
+
+sudo -u danny -i
+source ~/miniforge/etc/profile.d/conda.sh
+jupyter kernelspec list
+
+
+
